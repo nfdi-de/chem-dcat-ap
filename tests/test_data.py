898f0cb874a3de5bc3ceeb46f245db566a4868bd
Philip Strömert <Philip.Stroemert@tib.eu>
2025-03-11 11:22:06 +0100

    Update test_data.py to Copier template standard

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,31 +1,31 @@
 """Data test."""
 import os
 import glob
-import unittest
+import pytest
+from pathlib import Path
 
+import src.dcat_4c_ap.datamodel.dcat_4c_ap
 from linkml_runtime.loaders import yaml_loader
-from src.dcat_4c_ap.datamodel.dcat_4c_ap import NMRAnalysisDataset, Dataset, AnalysisDataset
-
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
 
 EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, '*.yaml'))
 
+DATA_DIR_VALID = Path(__file__).parent / "data" / "valid"
+DATA_DIR_INVALID = Path(__file__).parent / "data" / "invalid"
 
-class TestData(unittest.TestCase):
-    """Test data and datamodel."""
+VALID_EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR_VALID, '*.yaml'))
+INVALID_EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR_INVALID, '*.yaml'))
 
-    def test_data(self):
-        """Data test."""
-        for path in EXAMPLE_FILES:
-            print(path)
-            if os.path.basename(path) == 'Dataset-001.yaml':
-                obj = yaml_loader.load(path, target_class=Dataset)
-                assert obj
-            elif os.path.basename(path) == 'AnalysisDataset-001.yaml':
-                obj = yaml_loader.load(path, target_class=AnalysisDataset)
-                assert obj
-            elif os.path.basename(path) == 'NMRAnalysisDataset-001.yaml':
-                obj = yaml_loader.load(path, target_class=NMRAnalysisDataset)
-                assert obj
+
+@pytest.mark.parametrize("filepath", VALID_EXAMPLE_FILES)
+def test_valid_data_files(filepath):
+    """Test loading of all valid data files."""
+    target_class_name = Path(filepath).stem.split("-")[0]
+    tgt_class = getattr(
+        src.dcat_4c_ap.datamodel.dcat_4c_ap,
+        target_class_name,
+    )
+    obj = yaml_loader.load(filepath, target_class=tgt_class)
+    assert obj
