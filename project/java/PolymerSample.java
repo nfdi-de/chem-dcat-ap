4fd3d1b9cdcc7240540612e4e42b131f87b894e2
Philip Strömert <Philip.Stroemert@tib.eu>
2025-09-17 14:56:13 +0200

    Regen project with `just gen-project`

---
+++ project/java/PolymerSample.java
@@ -0,0 +1,19 @@
+package None;
+
+import java.util.List;
+import lombok.*;
+
+
+
+
+
+
+/**
+  A SubstanceSample derived from a Polymer.
+**/
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class PolymerSample extends SubstanceSample {
+
+
+}
